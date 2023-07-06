ca314f15d9e0730825ded74018281b25e5d6cc90
holycrab13 <jan.forberg@hotmail.de>
2023-07-06 10:08:38 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-while getopts s:t:a:o:g: flag
+while getopts s:t:a:o:g:c: flag
 do
     case "${flag}" in
         s) source=${OPTARG};;
@@ -6,6 +6,7 @@ do
         a) apiKey=${OPTARG};;
         o) offset=${OPTARG};;
         g) groups=${OPTARG};;
+        c) groups=${OPTARG};;
     esac
 done
 
@@ -19,4 +20,4 @@ chmod +x ./transfer.js
 npm install got
 npm install @frogcat/ttl2jsonld
 npm install rdf-parse
-node transfer.js -s $source -t $target -a $apiKey -o $offset -g $groups
+node transfer.js -s $source -t $target -a $apiKey -o $offset -g -c
